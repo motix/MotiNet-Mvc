088c5ed061f815b2547182048600d38cc3f11ef8
minh.nguyen@motiteam.com <minh.nguyen@motiteam.com>
2019-11-29 13:52:38 +0700

    Refresh untouched samples with ASP.NET Core 3.0

--- samples/UntouchedRazorPagesWithAuthentication/Pages/Error.cshtml.cs
+++ samples/UntouchedRazorPagesWithAuthentication/Pages/Error.cshtml.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace RazorPagesSample.Pages
+namespace UntouchedRazorPagesWithAuthentication.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public class ErrorModel : PageModel
@@ -14,6 +15,13 @@ namespace RazorPagesSample.Pages
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        private readonly ILogger<ErrorModel> _logger;
+
+        public ErrorModel(ILogger<ErrorModel> logger)
+        {
+            _logger = logger;
+        }
 
         public void OnGet()
         {
--- samples/UntouchedRazorPagesWithAuthentication/Program.cs
+++ samples/UntouchedRazorPagesWithAuthentication/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace UntouchedRazorPages
+namespace UntouchedRazorPagesWithAuthentication
 {
     public class Program
     {
